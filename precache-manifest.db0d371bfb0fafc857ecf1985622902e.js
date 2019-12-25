3c28348da075550928358e0200431bb6a7256a6d
Andrew Kou <andrewhkou@gmail.com>
2019-12-24 21:53:48 -0800

    Updates

---
+++ precache-manifest.db0d371bfb0fafc857ecf1985622902e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6318781b93eab2d0f16b09426523fe1f",
+    "url": "/andrewhkou.github.io/index.html"
+  },
+  {
+    "revision": "b87782b44dfe66d437a3",
+    "url": "/andrewhkou.github.io/static/css/main.af59e699.chunk.css"
+  },
+  {
+    "revision": "e282f8c6fb6a5fa41ff2",
+    "url": "/andrewhkou.github.io/static/js/2.1b65d898.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/andrewhkou.github.io/static/js/2.1b65d898.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b87782b44dfe66d437a3",
+    "url": "/andrewhkou.github.io/static/js/main.1da62153.chunk.js"
+  },
+  {
+    "revision": "f1ce722854310b7e0eef",
+    "url": "/andrewhkou.github.io/static/js/runtime-main.0891a3fb.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.78da449c4f9a03f1817feed849517808.js"
+  "/andrewhkou.github.io/precache-manifest.db0d371bfb0fafc857ecf1985622902e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/andrewhkou.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
